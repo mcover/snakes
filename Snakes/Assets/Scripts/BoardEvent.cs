a8880cb207500e65505cfd08421d4268a9be7d4a
fmmoret <fmmoret2@gmail.com>
2016-03-05 23:15:06 -0500

    pushed boardevent class

---
+++ Snakes/Assets/Scripts/BoardEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardEvent {
+
+    private Vector2 pos;
+    private List<BoardObject> objs;
+
+    public BoardEvent(Vector2 pos, List<BoardObject> objs)
+    {
+        this.pos = pos;
+        this.objs = objs;
+    }
+
+    public Vector2 getPos()
+    {
+        return pos;
+    }
+
+    public List<BoardObject> getObjs()
+    {
+        return objs;
+    }
+}
