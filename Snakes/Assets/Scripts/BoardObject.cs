f206061981405cac7a940cffcf1ff9e1bee60dc7
gueltro <gueltro@gmail.com>
2016-03-05 20:42:51 -0500

    Implemented getPositionAtTime and OnBoardAtTime for BoardObject and Snake

--- Snakes/Assets/Scripts/BoardObject.cs
+++ Snakes/Assets/Scripts/BoardObject.cs
@@ -17,6 +17,23 @@ public class BoardObject : MonoBehaviour {
 	private Vector2 startPos;
 
 	//returns list of all positions object takes at time t
-	public ArrayList<Vector2> getPosAtTime(int t){
+	public ArrayList<Vector2> getPositionAtTime(int t){
+		if (startPos == null) {
+			fprintf (stderr, "position of the gameObject is null .\n");
+			exit (-1);
+		}
+		// I bet there is a better way of creating story here
+		Vector2[] storyArray = new Vector2[][] {startPos};
+		ArrayList story = new ArrayList(storyArray);
+		return story;
+	}
+
+	//returns whether or not the BoardObject is still on the board
+	public bool onBoardAtTime(int t){
+		if (startPos == null) {
+			fprintf(stderr, "startPos is null;.\n");
+			exit(-1);
+		}
+		return true;
 	}
 }
--- Snakes/Assets/Scripts/Snake.cs
+++ Snakes/Assets/Scripts/Snake.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Snake : BoardObject {
@@ -22,21 +23,50 @@ public class Snake : BoardObject {
 
 	//return position of the head
 	public Vector2 getHead(){
-	
+		if (story == null || story.isEmpty()) {
+			fprintf(stderr, "story is null or empty;.\n");
+			exit(-1);
+		}
+		return story.get(story.Count - 1)
 	}
 
 	//add position to story
 	void moveTo(Vector2 pos){
-	
+		if (story == null || story.isEmpty()) {
+			fprintf(stderr, "story is null or empty;.\n");
+			exit(-1);
+		}
+		if (pos == null) {
+			fprintf(stderr, "Position is null.\n");
+			exit(-1);
+		}
+		story.Add(pos);
 	}
 
 	//return list of positions snake occupies at given time
+	//Note: assuming t starts at 0 at that snakes are instantiated with nonempty story
 	public ArrayList<Vector2> getPositionAtTime(int t){
-	
+		if (story == null || story.isEmpty()) {
+			fprintf(stderr, "story is null or empty;.\n");
+			exit(-1);
+		}
+		if (t > story.Count){
+			fprintf(stderr, "Inconsistency between query time and story length\n");
+			exit(-1);
+		}
+		int tailIndex = Math.Max(0, t - length);
+		return story.GetRange(tailIndex, t + 1)
 	}
 
 	//returns whether or not the snake is still on the board
+	// Note: only return false when the entire snake left the board
 	public bool onBoardAtTime(int t){
+		if (story == null || story.isEmpty()) {
+			fprintf(stderr, "story is null or empty;.\n");
+			exit(-1);
+		}
+		bool isOnBoard =  (story.Count + length <= t);
+		return isOnBoard;
 	}
 
 	//implement inherited vars and methods
