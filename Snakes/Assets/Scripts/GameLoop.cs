179481df7f624127994a3f832b066826c3f73eb9
Nayeon Kim <nayeonk@mit.edu>
2016-03-06 19:20:31 -0500

    Revert "adding keyboard events"
    This reverts commit 7cfad14a1a66efd78516832b1fac35d97520cba2.

--- Snakes/Assets/Scripts/GameLoop.cs
+++ Snakes/Assets/Scripts/GameLoop.cs
@@ -6,38 +6,11 @@ public class GameLoop : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Button for snake selection
-//		if(Input.GetButtonDown("snakeSelect"))
-//		{
-			// Probably something like buttonID
-//			Debug.Log(Input.mousePosition);
-//		}
-		if (Input.GetKeyUp (KeyCode.UpArrow)) {
-			Debug.Log("up key was released");
-			updateBoard ();
-		}
-		if (Input.GetKeyUp (KeyCode.DownArrow)) {
-			Debug.Log("down key was released");
-			updateBoard ();
-		}
-		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
-			Debug.Log("left key was released");
-			updateBoard ();
-		}
-		if (Input.GetKeyUp (KeyCode.RightArrow)) {
-			Debug.Log("right key was released");
-			updateBoard ();
-		}
-		if (Input.GetKeyUp (KeyCode.Alpha1)) {
-			Debug.Log("1 key was released");
-			// Possibly refresh game / refresh snake
-		}
-			
 
 	}
 
@@ -48,11 +21,6 @@ public class GameLoop : MonoBehaviour {
 	private List<Snake> allSnakes; //list of all snakes that exist in the puzzle
 	private List<BoardObject> puzzleObjects; //list of all objects inside the puzzle
 
-//	public GameLoop(int mapSize, List<Snake> allSnakes, List<BoardObject> gameObjects) {
-//		this.map = new Map(0, mapSize, mapSize);
-//		this.allSnakes = allSnakes;
-//		this.gameObjects = gameObjects;
-//	}
 
 	//take keyboard input somehow
 	//check if moving the current snake to the new position would be a valid move
