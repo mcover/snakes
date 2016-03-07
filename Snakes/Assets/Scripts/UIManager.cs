6e65742a9f2ba671e200220c2c79abdc385f8b0f
mcover <mcover@mit.edu>
2016-03-07 15:48:08 -0500

    Updated UI Stuff
    Changed buttons and added names to credits

--- Snakes/Assets/Scripts/UIManager.cs
+++ Snakes/Assets/Scripts/UIManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
     public Canvas startCanvas;
     public Canvas levelCanvas;
     public Canvas creditCanvas;
     public Canvas settingsCanvas;
+    public Canvas mainCanvas;
+    public Text helpText;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,8 @@ public class UIManager : MonoBehaviour {
         levelCanvas.enabled = false;
         creditCanvas.enabled = false;
         settingsCanvas.enabled = false;
+        mainCanvas.enabled = false;
+        helpText.enabled = false;
 	
 	}
 	
@@ -24,14 +29,14 @@ public class UIManager : MonoBehaviour {
     public void PlayGame()
     {
         //go to level screen
-        Debug.Log("play game");
+        //Debug.Log("play game");
         levelCanvas.enabled = true;
         //startCanvas.enabled = false;
     }
 
     public void EnableCredits()
     {
-        Debug.Log("enable credits");
+        //Debug.Log("enable credits");
         creditCanvas.enabled = true;
         //enable credits panel and button
     }
@@ -43,7 +48,7 @@ public class UIManager : MonoBehaviour {
     public void EnableSettings()
     {
         settingsCanvas.enabled = true;
-        Debug.Log("enable settings");
+        //Debug.Log("enable settings");
         //enable settings panel and buttons
     }
     public void DisableSetting()
@@ -51,14 +56,38 @@ public class UIManager : MonoBehaviour {
         settingsCanvas.enabled = false;
         //disable settings panel and buttons
     }
-    public void FromLevelToMain()
+    public void FromLevelToStart()
     {
-        //disable level canvas and enable main canvas
+        //disable level canvas and enable start canvas
         startCanvas.enabled = true;
         levelCanvas.enabled = false;
     }
     public void LoadLevel(int num)
     {
         //load level num
+        levelCanvas.enabled = false;
+        mainCanvas.enabled = true;
+    }
+    public void BackToMenu()
+    {
+        mainCanvas.enabled = false;
+        startCanvas.enabled = true;
+        levelCanvas.enabled = false;
+    }
+    public void ToggleHelpText()
+    {
+        if (helpText.enabled == false)
+        {
+            helpText.enabled = true;
+            Debug.Log("enabled help");
+            Debug.Log(helpText.enabled);
+        }
+        else if (helpText.enabled == true)
+        {
+            helpText.enabled = false;
+            Debug.Log("disabled help");
+            Debug.Log(helpText.enabled);
+        }
+
     }
 }
