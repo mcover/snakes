a04dba843a32f6906137b329f2caa35cf3c5d8b9
Nayeon Kim <nayeonk@mit.edu>
2016-03-11 13:36:49 -0500

    tiles scale to map dimensions

--- Snakes/Assets/Scripts/Tiles.cs
+++ Snakes/Assets/Scripts/Tiles.cs
@@ -16,42 +16,41 @@ public class Tiles: MonoBehaviour {
 
 	// private List<GameObject> mapTiles;
 		void drawEmptyBoard(int mapWidth, int mapHeight) {
-		//		GameObject tile = new GameObject();
-//		tile.tag = "Tile";
-//			for (int i = 0; i < mapWidth; i++) {
-//				for (int j = 0; j < mapHeight; j++) {
+		
+			for (int i = 0; i < mapWidth; i++) {
+				for (int j = 0; j < mapHeight; j++) {
 					GameObject tile = new GameObject();
 //                    tile.transform.parent = this.transform;
 					tile.transform.parent = this.gameObject.transform;
                     //tileCanvas.gameObject.;
 //				    GameObject newTile = this.gameObject.AddComponent<GameObject>("Tile");
 					Image tileImage = tile.AddComponent<Image> ();
+					
 					RectTransform rt = tileImage.rectTransform;
 					RectTransform panelRT = (RectTransform)this.gameObject.transform;
 
 					float width = rt.rect.width;
 					float height = rt.rect.height;
-
+					
 					float pWidth = panelRT.rect.width; // 400
 					float pHeight = panelRT.rect.height; // 400
+					float scaleRatio = (float)(pWidth/mapWidth)/width;
+					tileImage.transform.localScale = new Vector3 (scaleRatio,scaleRatio,1);
+					Debug.Log	("ratio");
+					Debug.Log (scaleRatio);
+					Debug.Log (pWidth/mapWidth);
 
 				    Sprite tileSprite = Resources.Load<Sprite>("completed_square") as Sprite;
-					Debug.Log (tile.gameObject.transform.position);
 				    tileImage.sprite = tileSprite;
                 
 					// transform.rotation not necessary untill handling boardObjects
-					Vector3 tilePos = new Vector3(0,0,0);
-//					Vector3 tilePos = new Vector3(i*tileWidth,j*tileHeight,0); //shouldn't this be related to the total space the board has?
-					Vector3 panelWidthOffset = new Vector3(-pWidth/2,pHeight/2,0);
-					Vector3 panelOffset = this.gameObject.transform.position + panelWidthOffset;
-                	//Debug.Log ("panel size"+this.gameObject.transform.position.ToString());	
-                    //tile.transform.position = newTilePos;
-					Vector3 offset = new Vector3 (width/2, -height/2,0);
-//					tile.transform.position = tilePos + offset; 
-//					tile.transform.localScale = new Vector3 (1,1,1);
-					tile.transform.localPosition = tilePos + panelWidthOffset + offset;
-//				}
-//			}
+					Vector3 tilePos = new Vector3(i*width*scaleRatio,j*height*scaleRatio,0);
+					Vector3 panelOffset = new Vector3(-pWidth/2,-pHeight/2,0);
+					Vector3 tileOffset = new Vector3 (width*scaleRatio/2, -height*scaleRatio/2,0);
+					
+					tile.transform.localPosition = tilePos + panelOffset + tileOffset;
+				}
+			}
 		}
 
 
